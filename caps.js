eae8aa8774e8a2cc5478c1d3e925f02f997c6ee3
alexrpedersen <alex.r.pedersen@outlook.com>
2020-05-05 17:42:52 -0700

    adds in our socket id

--- caps.js
+++ caps.js
@@ -1,6 +1,13 @@
+'use strict';
+
+const net = require('net');
 const events = require('./events.js');
 const vendor = require('./vendor.js');
 const driver = require('./driver.js');
+const PORT = process.env.PORT||3000
+const server = net.createServer();
+
+let socketPool = {};
 
 events.on('pickup', payload => console.log('The "package" has been picked up', payload));
 events.on('inTransit', payload => console.log('The package is in transit', payload));
@@ -10,3 +17,29 @@ function log(event, payload) {
   let timestamp = new Date();
   console.log('LOG:', {payload})
 }
+
+server.on('connection', (socket) => {
+  console.log('client:', socket);
+  const id = `Socket-${Math.random()}`;
+  socketPool[id] = socket;
+
+  socket.on('data', (buffer) => {
+    //dispatch function 
+  });
+});
+
+function dispatchEvent(buffer) {
+  let message = JSON.parse(buffer.toString().trim());
+  broadcaast(message);
+}
+
+function broadcast(message) {
+  let payload = JSON.stringify(message);
+  for (let socket in socketPool) {
+    socketPool[socket].write(payload);
+  }
+}
+
+server.listen(PORT, () => {
+  console.log('Server is up on ${PORT}');
+});
